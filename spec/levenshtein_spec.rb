1d6aafa473d3622c10b031b775bbff5038b5a79d
Albert Segarra <albertsgrc@gmail.com>
2021-05-02 22:01:46 +0200

    test: fix

--- spec/levenshtein_spec.rb
+++ spec/levenshtein_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe Levenshtein do
   fixtures = [
-    ["hello", "hello", 0],
-    ["hello", "helo", 1],
-    ["hello", "jello", 1],
-    ["hello", "helol", 1],
-    ["hello", "hellol", 1],
-    ["hello", "heloll", 2],
-    ["hello", "cheese", 4],
-    ["hello", "saint", 5],
-    ["hello", "", 5],
+    [[1], [1], 0],
+    [[1], [2], 1],
+    [[1], [3], 1],
+    [[1], [4], 1],
+    [[1], [5], 1],
+    [[1], [6, 3], 2],
+    [[1], [7, 3, 4, 1], 3],
+    [[1], [8, 2, 3, 3, 3], 5],
+    [[1], [9, 2, 3, 5, 6], 5],
   ]
 
   fixtures.each do |w1, w2, d|
@@ -21,13 +21,13 @@ describe Levenshtein do
   end
 
   it "should raise an error if either argument is nil" do
-    expect { Levenshtein.distance("", nil) }.to raise_error TypeError
-    expect { Levenshtein.distance(nil, "") }.to raise_error TypeError
+    expect { Levenshtein.distance([1], nil) }.to raise_error TypeError
+    expect { Levenshtein.distance(nil, [1]) }.to raise_error TypeError
   end
 
-  it "should raise an error if either argument is something else than a string" do
-    expect { Levenshtein.distance("woah", /woah/) }.to raise_error TypeError
-    expect { Levenshtein.distance(5.3, "5.3") }.to raise_error TypeError
-    expect { Levenshtein.distance(Object.new, "Hello") }.to raise_error TypeError
+  it "should raise an error if either argument is something else than an array of strings" do
+    expect { Levenshtein.distance([2], /woah/) }.to raise_error TypeError
+    expect { Levenshtein.distance(5.3, [3]) }.to raise_error TypeError
+    expect { Levenshtein.distance(Object.new, [1]) }.to raise_error TypeError
   end
 end
